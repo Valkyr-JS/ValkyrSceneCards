80452b4f58beb924c3fb31f564495785ecd9e2f8
ValkyrJS <valkyr.photos@gmail.com>
2024-07-03 00:01:34 +0100

    Remade title

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ const { React } = PluginApi;
 
 // Remove overlays
 PluginApi.patch.instead("SceneCard.Details", function (props) {
-  console.log(props);
+  const link = `/scenes/${props.scene.id}`;
   return [
     <div className="scene-card__details">
-      <span className="scene-card__date">{props.scene.date}</span>
+      <a href={link}>
+        <h5 className="card-section-title flex-aligned">{props.scene.title}</h5>
+      </a>
       <div className="scene-card__description">{props.scene.details}</div>
+      <span className="scene-card__date">{props.scene.date}</span>
     </div>,
   ];
 });
