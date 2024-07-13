aed983b7fc553959a38e277247cb89c7813e3caa
ValkyrJS <valkyr.photos@gmail.com>
2024-07-13 10:57:32 +0100

    Performer text list ages

--- src/main.tsx
+++ src/main.tsx
@@ -11,9 +11,9 @@ const { GQL, React } = PluginApi;
 PluginApi.patch.instead("SceneCard", function (props, _, Original) {
   // Fetch user plugin config
   const qConfig = GQL.useConfigurationQuery();
-  const dataLoaded = !qConfig.loading;
+  const configLoaded = !qConfig.loading;
 
-  if (dataLoaded) {
+  if (configLoaded) {
     const userConfig: VSCConfigMap =
       qConfig.data.configuration.plugins.ValkyrSceneCards;
 
@@ -37,6 +37,26 @@ PluginApi.patch.instead("SceneCard", function (props, _, Original) {
 
     // Fetch additional data as needed
     const extendedProps: ISceneCardPropsExtended = { ...props, config };
+
+    // Performer data
+    if (!!extendedProps.scene.performers) {
+      const performersData = GQL.useFindPerformersQuery({
+        variables: {
+          filter: { per_page: -1 },
+          performer_ids: extendedProps.scene.performers.map((pf) => +pf.id),
+        },
+      });
+
+      if (!!performersData.data)
+        extendedProps.scene = {
+          ...extendedProps.scene,
+          performers: performersData.data.findPerformers.performers,
+        };
+
+      console.log(extendedProps);
+    }
+
+    // Studio data
     if (!config.hideParentStudio && !!extendedProps.scene.studio) {
       const studioData = GQL.useFindStudioQuery({
         variables: { id: props.scene.studio?.id || "" },
